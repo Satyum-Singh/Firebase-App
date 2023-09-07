ac89f07b6aeb5ad5b08f6ee98b5bc7a8102baac5
Satyum <satyumspathania@gmail.com>
2023-09-07 15:18:12 +0530

    setting up the assests.

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById("input-field");
+const addbtn = document.getElementById("add-btn");
+
+addbtn.addEventListener("click", function () {
+  let inputValue = inputField.value;
+  console.log(inputValue);
+});
