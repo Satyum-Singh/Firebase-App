be0c4ae6fd96f0fcb837d9395b69d0c295b84969
Satyum <satyumspathania@gmail.com>
2023-09-07 16:05:37 +0530

    Setting firebase with project.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const appSettings = {
+  databaseURL:
+    "https://myfirstapp-3084e-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
 const inputField = document.getElementById("input-field");
 const addbtn = document.getElementById("add-btn");
 
